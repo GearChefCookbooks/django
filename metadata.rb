b2915c9789281c05d3a054978ee9780c36d5db74
Gary Leong <gwleong@gmail.com>
2015-01-28 12:11:58 -0800

    first commit

---
+++ metadata.rb
@@ -0,0 +1,14 @@
+name             'django'
+maintainer       'Gary Leong'
+maintainer_email 'gwleong@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures django'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "apache2"
+depends "apt"
+depends "git"
+depends "vim"
+depends "python"
+depends "build-essential"
